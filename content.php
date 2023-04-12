fc58b99889c7e3c30ac9f31490dd4fe17e24f335
Reza <99068159@mydavinci.nl>
2023-04-12 11:50:02 +0200

    Heb de file nu zo gemaakt dat alles er sowieso 1 keer op staat

---
+++ content.php
@@ -0,0 +1,10 @@
+<?php
+
+$VolleNaam = "Reza van Wegberg"
+
+
+?>
+
+<img src="elephant.png" alt="kleine olifant image">
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium dolores eaque aspernatur aut facilis veniam? In tempore nihil maxime molestiae distinctio, perspiciatis blanditiis id porro voluptatem mollitia nulla cum reprehenderit non et, aperiam nemo eveniet, a ea debitis qui impedit cupiditate velit voluptatum. Sunt harum odit hic, atque voluptas accusantium!</p>
