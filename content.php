912d07798538951c1bf87679d8e273d919c1d987
Reza <99068159@mydavinci.nl>
2023-04-12 12:20:51 +0200

    Heb een loop kunnen zetten in de php file die ge include wordt en heb daarmee de html in een for loop kunnen zetten

--- content.php
+++ content.php
@@ -2,9 +2,9 @@
 
 $VolleNaam = "Reza van Wegberg"
 
+for($i = 0; $i < 10; $i++) {
+    echo '<p>Lorem ipsum dolor sit</p>';
+    echo '<img src="elephant.png" alt="elephant image">';
+}
 
 ?>
-
-<img src="elephant.png" alt="kleine olifant image">
-
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium dolores eaque aspernatur aut facilis veniam? In tempore nihil maxime molestiae distinctio, perspiciatis blanditiis id porro voluptatem mollitia nulla cum reprehenderit non et, aperiam nemo eveniet, a ea debitis qui impedit cupiditate velit voluptatum. Sunt harum odit hic, atque voluptas accusantium!</p>
--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <title>Document</title>
 </head>
 <body>
+
     <?php include 'content.php'; ?>
+    
 </body>
 </html>
